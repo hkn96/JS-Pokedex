c3ed6e969ea254e80c1a4e8509369f842c81cb2c
Hakan Ünlü <unluhak@outlook.com>
2021-10-21 00:05:26 +0200

    Css--grid-google fonts-animation erstellt

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const pokedex = document.getElementById('pokedex');
+console.log(pokedex);
+
 const fetchPokemon = () => {
   const promises = [];
   for (let i = 1; i <= 150; i++) {
@@ -18,6 +21,19 @@ const fetchPokemon = () => {
 
 const displayPokemon = pokemon => {
   console.log(pokemon);
+  const pokemonHTMLString = pokemon
+    .map(
+      pokeman => `
+ <li class="card">
+     <img class="card-image" src="${pokeman.image}"/>
+     <h2 class="card-title">${pokeman.id}.${pokeman.name}</h2>
+     <p class="card-subtitle">Type : ${pokeman.type}</p>
+
+ </li>
+ `
+    )
+    .join('');
+  pokedex.innerHTML = pokemonHTMLString;
 };
 
 fetchPokemon();
